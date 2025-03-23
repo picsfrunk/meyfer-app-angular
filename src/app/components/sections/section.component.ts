fbb4b31f42b83950b3e85dc852659a79aecd5966
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2025-03-23 02:52:34 -0300

    indice mudado a section

--- src/app/components/sections/section.component.ts
+++ src/app/components/sections/section.component.ts
@@ -12,4 +12,17 @@ import { ProductComponent } from '../product/product.component';
 })
 export class SectionComponent {
   @Input() section!: Section;
+  @Input() allSections!: any[];
+
+  get otherSections() {
+    return this.allSections.filter((s) => s.id !== this.section.id);
+  }
+
+  scrollToSection(id: string, event: Event) {
+    event.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'instant' });
+    }
+  }
 }
