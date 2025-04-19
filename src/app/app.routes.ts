140fca1b660a501279b9369fbc3190a12dae5937
Alejandro Nava <101224369+picsfrunk@users.noreply.github.com>
2025-04-19 19:58:47 -0300

    del tests and unused components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
 import {CatalogComponent} from './components/catalog/catalog.component';
-import {DataUploadComponent} from './components/data-upload/data-upload.component';
 
 export const routes: Routes = [
   { path: '', component: CatalogComponent },
-  { path: 'upload', component: DataUploadComponent },
 ];
