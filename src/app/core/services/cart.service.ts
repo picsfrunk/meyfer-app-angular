4e9ce680efd53b21a1cdb509ada821e052387b71
Alejandro Nava <picsfrunk@gmail.com>
2025-08-19 20:48:06 -0300

    import ent Product en cartService

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Product } from '../models/product.model';
 
 export interface CartItem {
   product: Product;
