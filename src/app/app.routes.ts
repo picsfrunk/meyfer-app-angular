239e25c854605c24c18cec21ea52dad78c36971b
Alejandro Nava <picsfrunk@gmail.com>
2025-08-30 10:57:58 -0300

    chanche products component path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {Products} from './pages/product-list/products';
+import {Products} from './pages/products/products';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
--- src/app/pages/welcome/welcome.routes.ts
+++ src/app/pages/welcome/welcome.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Welcome } from './welcome';
-import {Products} from '../product-list/products';
 
 export const WELCOME_ROUTES: Routes = [
   { path: '', component: Welcome },
