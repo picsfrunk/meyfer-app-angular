22ca8a0d150cb36363eaaaa702418570110bbd58
Alejandro Nava <picsfrunk@gmail.com>
2025-09-08 21:58:51 -0300

    nz drawer implement on menu

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -3,6 +3,9 @@ import {CartBadge} from '../shared/cart-badge';
 import {NzContentComponent, NzHeaderComponent, NzLayoutComponent, NzSiderComponent} from 'ng-zorro-antd/layout';
 import {RouterOutlet} from '@angular/router';
 import {Menu} from '../core/components/sidebar/menu.component';
+import {NzDrawerModule} from 'ng-zorro-antd/drawer';
+import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-layout',
@@ -13,11 +16,19 @@ import {Menu} from '../core/components/sidebar/menu.component';
     NzLayoutComponent,
     NzSiderComponent,
     RouterOutlet,
-    Menu
+    Menu,
+    NzDrawerModule,
+    NzIconDirective,
+    NzButtonComponent
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
+  isMobileMenuOpen = false;
+
+  close(): void {
+    this.isMobileMenuOpen = false;
+  }
 
 }
