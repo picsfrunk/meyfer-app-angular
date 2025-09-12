d2041c51a777d9fd4ba711f08d82f00da1aa3546
Alejandro Nava <picsfrunk@gmail.com>
2025-09-11 21:52:37 -0300

    staging config on pr

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://meyfer-backend-expressjs-development.up.railway.app/',
+  locale: 'es-AR',
+  currency: 'ARS',
+  currencySymbol: '$'
+};
