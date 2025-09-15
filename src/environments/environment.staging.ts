fb331d17f9a6040ad9d66da670aa895c17ade737
Alejandro Nava <picsfrunk@gmail.com>
2025-09-15 19:30:59 -0300

    fixes

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://meyfer-backend-expressjs-development.up.railway.app/',
+  apiUrl: 'https://meyfer-backend-expressjs-development.up.railway.app/api',
   locale: 'es-AR',
   currency: 'ARS',
   currencySymbol: '$'
