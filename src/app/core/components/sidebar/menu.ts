921c8df29d4747b419f5079a3bef90ff85d2983a
Alejandro Nava <picsfrunk@gmail.com>
2025-11-03 20:33:18 -0300

    serch reset on category select

--- src/app/core/components/sidebar/menu.ts
+++ src/app/core/components/sidebar/menu.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Component, signal, OnInit, inject, computed, Input, WritableSignal} from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import {MenuService, NzMenuModule} from 'ng-zorro-antd/menu';
-import { NzSpinComponent } from 'ng-zorro-antd/spin';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzMenuModule} from 'ng-zorro-antd/menu';
+import {NzSpinComponent} from 'ng-zorro-antd/spin';
 import {CategoryService} from '../../services/category.service';
 import {MENU_ITEMS} from './menu-items';
 import {MenuItem} from '../../models/menu-item.model';
@@ -37,22 +37,22 @@ export class Menu implements OnInit {
 
   onCategorySelected = (category: Category) => {
     this.productService.selectedCategory.set(category);
-    let queryParams: any;
 
-    if (!category.category_id) {
-      queryParams = { category_id: null };
-    } else {
-      queryParams = { category_id: category.category_id, page: 1 };
-    }
+    let queryParams: any = {
+      category_id: category.category_id || null,
+      page: 1,
+      search: null
+    };
 
     this.router.navigate(['/products'], {
       queryParams,
       queryParamsHandling: 'merge'
     });
+
     this.isMobileMenuOpen.set(false);
   };
 
-  fullCategories = computed( () => {
+  fullCategories = computed(() => {
     const allCategory: Category = {
       category_id: 0,
       category_name: 'Todos',
